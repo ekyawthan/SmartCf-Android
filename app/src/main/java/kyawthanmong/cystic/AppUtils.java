72b6e33603db4c2f7c6b814431688a6bf182239d
kyaw Mong <ekyawthan@gmail.com>
2015-05-20 11:10:01 -0400

    init commit

---
+++ app/src/main/java/kyawthanmong/cystic/AppUtils.java
@@ -0,0 +1,15 @@
+package kyawthanmong.cystic;
+
+import android.content.Context;
+
+/**
+ * Created by kyawthan on 5/20/15.
+ */
+public class AppUtils {
+
+    private Context context;
+
+    public AppUtils(Context context){
+        this.context = context;
+    }
+}
---
+++ app/src/main/java/kyawthanmong/cystic/LoginActivity.java
@@ -0,0 +1,38 @@
+package kyawthanmong.cystic;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.rengwuxian.materialedittext.MaterialEditText;
+
+
+public class LoginActivity extends ActionBarActivity {
+
+    private MaterialEditText userId, userPassword;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        shouldSetupView();
+    }
+
+
+    private void shouldSetupView() {
+
+        userId = (MaterialEditText) findViewById(R.id.userId);
+        userPassword = (MaterialEditText) findViewById(R.id.password);
+
+
+
+    }
+
+
+    public void didClickOnSignIn(View view)
+    {
+
+    }
+
+}
